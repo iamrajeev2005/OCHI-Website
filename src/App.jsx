e20ef1a902e49994d6056033c72b4cda5fc84cb2
iamrajeev2005 <fea.rajeev@gmail.com>
2024-02-06 22:21:26 +0530

    project setup and making navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="h-screen w-full bg-zinc-100">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
