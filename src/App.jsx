517a625fa3e802fb6faa88b404fc41034d09d967
iamrajeev2005 <fea.rajeev@gmail.com>
2024-02-07 15:41:43 +0530

    locomotive added

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,21 @@ import Hero from "./components/Hero";
 import Marquee from "./components/Marquee";
 import About from "./components/About";
 import Play from "./components/Play";
+import Featured from "./components/Featured";
+import LocomotiveScroll from "locomotive-scroll";
 
 function App() {
+  
+  const locomotiveScroll = new LocomotiveScroll();
+
   return (
     <div className="h-screen w-full  relative">
       <Navbar />
-      <Hero/>
-      <Marquee/>
-      <About/>
-      <Play/>
+      <Hero />
+      <Marquee />
+      <About />
+      <Play />
+      <Featured />
     </div>
   );
 }
---
+++ src/components/Featured.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Featured() {
+  return (
+    <div className="w-full h-screen bg-zinc-100  pt-28">
+      <h1 className="text-5xl font-[NU] px-12 text-[#181616]">
+        Featured projects
+      </h1>
+      <div className="w-full border-t-[1px] border-[#9c9c9c] mt-10"></div>
+    </div>
+  );
+}
+
+export default Featured;
